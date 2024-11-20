6b11b9e040e55b41df4832c782e6a9371c813ee5
AmirAhmed0s <163478540+AmirAhmed0s@users.noreply.github.com>
2024-11-20 20:24:34 +0200

    Update final_settlement.py

--- alpha_deem/alphadeem/doctype/final_settlement/final_settlement.py
+++ alpha_deem/alphadeem/doctype/final_settlement/final_settlement.py
@@ -4,63 +4,80 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils.data import date_diff
+
 class FinalSettlement(Document):
     def validate(self):
         self.calc_settlement_totals()
-        
 
     def calc_settlement_totals(self):
-        earning=0
-        deduction=0
+        earning = 0
+        deduction = 0
         for entitlements in self.entitlements:
-               earning+=entitlements.amount
-        self.total_entitlements=earning
+            earning += entitlements.amount
+        self.total_entitlements = earning
 
         for deductions in self.deductions:
-            deduction+=deductions.amount 
-        self.total_deductions=deduction    
-       
+            deduction += deductions.amount
+        self.total_deductions = deduction
 
+        self.total_settlement = earning - deduction
 
-        self.total_settlement=earning-deduction
-
-
-
-    
     @frappe.whitelist()
     def calc_service_duration(self):
-        diff_data=get_date_diff(self.last_day_of_work,self.date_of_joining)
-        self.service_duration =diff_data
+        diff_data = get_date_diff(self.last_day_of_work, self.date_of_joining)
+        self.service_duration = diff_data
 
-        diff_data_last_days=get_date_diff(self.last_day_of_work,self.actual_start_date)
-        self.last_working_period =diff_data_last_days
+        diff_data_last_days = get_date_diff(self.last_day_of_work, self.actual_start_date)
+        self.last_working_period = diff_data_last_days
 
-        
         try:
-            doc = frappe.get_doc('Salary Structure Assignment',  { "docstatus": 1,"employee":self.employee })
-            self.basic_salary=doc.base
-            self.housing_allowance= doc.housing_allowance
-            self.other_allowances= doc.other_allowances
-            self.nature_of_work=doc.nature_of_work
-            self.transportation_allowance= doc.transportation_allowance
-            self.extra_work=doc.extra_work
+            # Determine if the employee has a clearance form
+            clearance_form = frappe.db.get_value(
+                "Clearance Form", 
+                {"employee": self.employee, "docstatus": 1}, 
+                "name"
+            )
+
+            if not clearance_form:
+                # Fetch data directly from Employee
+                employee_doc = frappe.get_doc("Employee", self.employee)
+                self.basic_salary = employee_doc.basic_salary
+                self.housing_allowance = employee_doc.housing_allowance
+                self.other_allowances = employee_doc.other_allowances
+                self.nature_of_work = employee_doc.nature_of_work
+                self.transportation_allowance = employee_doc.transportation_allowance
+                self.extra_work = employee_doc.extra_work
+            else:
+                # Fetch data from Clearance Form
+                clearance_doc = frappe.get_doc("Clearance Form", clearance_form)
+                self.basic_salary = clearance_doc.basic_salary
+                self.housing_allowance = clearance_doc.housing_allowance
+                self.other_allowances = clearance_doc.other_allowances
+                self.nature_of_work = clearance_doc.nature_of_work
+                self.transportation_allowance = clearance_doc.transportation_allowance
+                self.extra_work = clearance_doc.extra_work
         except frappe.DoesNotExistError:
             pass
 
-        current_doc=self.as_dict()
-        fields=["basic_salary","housing_allowance","other_allowances","nature_of_work","transportation_allowance","extra_work"]
-        total=0
+        current_doc = self.as_dict()
+        fields = [
+            "basic_salary", 
+            "housing_allowance", 
+            "other_allowances", 
+            "nature_of_work", 
+            "transportation_allowance", 
+            "extra_work"
+        ]
+        total = 0
         for field in fields:
-            total+=current_doc[field]
-        self.total_salary= total
+            total += current_doc.get(field, 0)
+        self.total_salary = total
 
 
-def get_date_diff(first_date,second_date):
-    days=date_diff(first_date,second_date)
-    date=days_to_years_months_days(days)
-    return "  سنه   {0}   شهر   {1}    يوم    {2}".format(date[0],date[1],date[2])
-
-
+def get_date_diff(first_date, second_date):
+    days = date_diff(first_date, second_date)
+    date = days_to_years_months_days(days)
+    return "  سنه   {0}   شهر   {1}    يوم    {2}".format(date[0], date[1], date[2])
 
 
 def days_to_years_months_days(days):
